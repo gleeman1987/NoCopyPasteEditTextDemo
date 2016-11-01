990722877b4f925f1bc32cdb0c0a29061faffb86
郑军 <zhengjun1987@outlook.com>
2016-11-02 01:20:36 +0800

    2016年11月02日01:17提交

---
+++ app/src/main/java/com/example/zhengjun/nocopypasteedittextdemo/NoCopyPasteEditText.java
@@ -0,0 +1,100 @@
+package com.example.zhengjun.nocopypasteedittextdemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+/**
+ * Created by zhengjun on 16/11/2.
+ */
+public class NoCopyPasteEditText extends EditText {
+
+    private Context context;
+
+    public NoCopyPasteEditText(Context context) {
+        super(context);
+        this.context = context;
+        setLongClickable(false);
+        setTextIsSelectable(false);
+        setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        });
+    }
+
+    public NoCopyPasteEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        setLongClickable(false);
+        setTextIsSelectable(false);
+        setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        });
+    }
+
+    public NoCopyPasteEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.context = context;
+        setLongClickable(false);
+        setTextIsSelectable(false);
+        setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        });
+    }
+}
